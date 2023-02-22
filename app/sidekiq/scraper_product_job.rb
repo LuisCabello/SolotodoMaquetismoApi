e654ef58fafe10db0f8ca5a51f9df9cb8898ddb2
Luis Felipe Cabello Alvarez <luis.cabello.a@mail.pucv.cl>
2023-02-22 15:50:16 -0300

    Avance y mejoras

--- app/sidekiq/scraper_product_job.rb
+++ app/sidekiq/scraper_product_job.rb
@@ -1,49 +1,78 @@
-# Paso 5
-# Toma cada producto y lo setea, lo deja listo para guardar la informacion en la base de datos
+# Step 5 Fin
+# Take each product and set it up, leave it ready to save the information in the database
 
 class ScraperProductJob
   include Sidekiq::Job
+  include MiraxHelper
 
   require 'nokogiri'
   require 'open-uri'
   require 'json'
   require 'csv'
 
-  # HAY QUE MENEJAR LA CATEGORIA AQUI!
-  def perform(url)
+  def perform(url, type, category_name, _store_id)
     html  = URI.open(url).read
     doc   = Nokogiri::HTML(html)
     image = ''
+    category_exists = false
 
-    # Hash to save data
-    product = { 'name' => ' ', 'price' => ' ', 'offer_price' => ' ', 'type' => ' ', 'image' => ' ',
-                'creation_date' => ' ', 'category' => ' ', 'brand' => ' ' }
-    # Hash to save json description data
+    # Set $type_values
+    type_values = global_variables(category_name)
+
+    # Hash to save data into Product Model
+    product = { 'name' => ' ', 'type' => ' ', 'image' => ' ', 'creation_date' => ' ', 'category_id' => ' ' }
+    # Hash to save json description data Product Model
     description = { 'description' => '', 'scale' => '' }
+
+    #  Hash to save data into Price Model
+    price = { 'product_id' => ' ', 'store_id' => ' ', 'amount' => ' ', 'offer_amount' => ' ', 'current_amount' => ' ' }
+
+    # Get the category and the id
+    @category_filter = Category.new
+    final_category = @category_filter.getCategoryByName(category_name)
+
+    # Revisar bien lo de manejo de errores
+    if final_category.present?
+      product['category_id'] = final_category.id
+    else
+      # Handle the error
+      raise StandardError, "No se encontró ninguna categoría con nombre #{category_name}"
+      # Set the category to "Otros"
+      # categories_by_name = @category_filter.getCategoryByName("Otros")
+      # product['category_id'] = categories_by_name.id
+    end
 
     doc.search('.row h1').map do |element|
       product['name'] = element.inner_text.strip
     end
 
     doc.search('.price').map do |element|
-      product['price'] = element.inner_text.strip
+      price['amount'] = element.inner_text.strip
     end
 
-    #------------------- Description
+    # En caso de que no se identifique la categoria se deja como "otros"
+    if type.blank?
+      type = 'Otros'
+    else
+      type.downcase!
 
-    # Dejar para despues
-    # # Description for the json 
-    # doc.search('span').map do |element|
-    #   description['description'] = element.inner_text.strip
-    # end
+      type_values.each do |key, value|
+        value.each do |stringValue|
+          next unless type.include?(stringValue)
 
-    # # Hay que pasarlas a un jsonb
-    # # categoria, tipo, escala
-    # doc.search('.breadcrumb').map do |element|
-    #   element.inner_text.strip
-    # end
+          type = key
+          category_exists = true
+          break
+        end
+      end
+    end
 
-    #------------------- 
+    product['type'] = type
+
+    # Get Description for the json
+    doc.search('#descr').map do |element|
+      description['description'] = element.inner_text.strip
+    end
 
     # Get Img UrL
     doc.css('img').map do |element|
@@ -60,16 +89,21 @@ class ScraperProductJob
     values = time.to_a
     product['creation_date'] = Time.utc(*values)
 
-    nameProduct = product['name']
+    name_product = product['name']
 
-    # Get the brand
-    brand = nameProduct.partition(' ').first
+    # Get the brand name
+    brand_name = name_product.partition(' ').first
+
+    # Get the Brand and the id
+    @brand = Brand.new
+    final_brand =  @brand.getBrandByName(brand_name)
 
     # String to array
-    nameProduct = nameProduct.split
+    name_product = name_product.split
 
+    # !!!! TAL VEZ SI NO ENCUENTRA ESCALA QUE QUEDE COMO OTROS? PERO QUE TENGA QUE SER MAQUETA
     # Get the scale from the name product
-    nameProduct.each do |x|
+    name_product.each do |x|
       if x.include?(':')
         description['scale'] = x
         break
@@ -77,29 +111,31 @@ class ScraperProductJob
     end
 
     # Clean the name of the product
-    nameProduct.delete_if { |x| x.include?(':') }
-    nameProduct.delete_if { |x| x.include?(brand) }
+    name_product.delete_if { |x| x.include?(':') }
+    name_product.delete_if { |x| x.include?(brand_name) }
 
     # Final name of the product
-    nameProduct = nameProduct.join(' ')
-    product['name'] = nameProduct
+    name_product = name_product.join(' ')
+    product['name'] = name_product
 
     # if there is no data it is not shown
     # description = description.to_json
 
-    # Hay datos que no van en el modelo Product, si no que van en otro modelo
-    # Para las relaciones con las otras clases tenemos que ver como conseguir sus ids
-
-    # Create a new product and add the data to it, then we save it in the database
+    # Save a new Product
     # final_product = Product.new
-    # final_product.name = product["name"]
-    # # final_product.price = product["price"]
-    # # final_product.offer_price = product["offer_price"]
-    # final_product.type = product["type"]
-    # final_product.image = product["image"]
-    # final_product.creation_date = product["creation_date"]
-    # # final_product.category = product["category"]
-    # # final_product.brand = product["brand"]
-    # final_product.save
+    # final_product = final_product.addProduct(product['name'], description, type, product['image'], product['creation_date'],
+    #                                          product['category_id'])
+    # final_product.categories << final_category
+
+    # # Save a new Price
+    # final_price = Price.new
+    # final_price = final_price.addPrice(final_product.id, store_id, price['amount'].delete('^0-9').to_i,
+    #                                    price['offer_amount'], price['current_amount'])
+
+    # # print(final_price.errors.full_messages)
+
+    # # Save a new data into the middle table brands_products
+    # final_product.brands << final_brand
   end
 end
+# Step 5 Fin
